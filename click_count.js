3d1751f13ec6f013fdcecdb4ba13f2aecfd6d588
LeopardCheetah <68766450+LeopardCheetah@users.noreply.github.com>
2023-11-10 15:55:56 -0800

    button testing

--- click_count.js
+++ click_count.js
@@ -1,6 +1,9 @@
 var clicks = 0;
+var button = document.getElementbyID("click_button");
+
+
 
 function click_count() {
   clicks += 1;
-  document.getElementById("clicks").innerHTML = clicks;
+  document.getElementById("clicks").innerText = clicks;
 };
