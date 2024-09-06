1d85017ef5159e8d542c19b5a06d41e64c1593cd
LeopardCheetah <hawkswithmath@gmail.com>
2024-09-05 17:28:19 -0700

    added backticks

--- click_count.js
+++ click_count.js
@@ -2,11 +2,14 @@ let clicks = 0;
 let button = document.getElementById('click_button');
 let count = document.getElementById('count');
 
-count.innerText = 'Clicks: ${clicks}'; // think python f strings here
+
+// think python f strings here
+// need backticks here
+count.innerText = `Clicks: ${clicks}`; 
 
 
 button.addEventListener('click', () => {
   console.log(clicks);
   clicks++;
-  count.innerText = 'Clicks: ${clicks}';
+  count.innerText = `Clicks: ${clicks}`;
 })
